1437a502fd8aedb3410d5e6199dd8d30b84866f7
Valentina Aquino <valentinaaquino@valentinas-air.t-com.me>
2022-05-28 13:20:01 +0200

    Prepare deploy

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -42,16 +42,15 @@ const getGoals = asyncHandler (async (req, res) => {
          throw new Error('Goal not found')
      }
 
-     const user = await User.findById(req.user.id)
 
      //Chech for user 
-     if(!user) {
+     if(!req.user) {
          res.status(401)
          throw new Error('User not found')
      }
 
      //Make sure the logged in user matches the goal user 
-     if(goal.user.toString() !== userid) {
+     if(goal.user.toString() !== req.user.id) {
          res.status(401)
          throw new Error('User not authorized')
      }
@@ -73,16 +72,14 @@ const getGoals = asyncHandler (async (req, res) => {
         throw new Error('Goal not found')
     }
 
-    const user = await User.findById(req.user.id)
-
      //Chech for user 
-     if(!user) {
+     if(!req.user) {
          res.status(401)
          throw new Error('User not found')
      }
 
      //Make sure the logged in user matches the goal user 
-     if(goal.user.toString() !== userid) {
+     if(goal.user.toString() !== req.user.id) {
          res.status(401)
          throw new Error('User not authorized')
      }
